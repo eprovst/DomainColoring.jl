4f8413212d761d09936855ea1d650098f09c8c44
Evert Provoost <evert.provoost@kuleuven.be>
2023-07-07 14:48:42 +0200

    Documented internal functions

--- src/DomainColoring.jl
+++ src/DomainColoring.jl
@@ -1,13 +1,14 @@
-# DomainColoring.jl
-#
-# Copyright (c) 2023 Evert Provoost. See LICENSE.
-#
-#
-# Provided functionality is partially inspired by
-#
-#   Wegert, Elias. Visual Complex Functions: An Introduction with
-#   Phase Portraits. Birkhäuser Basel, 2012.
-#
+#==
+ = DomainColoring.jl
+ =
+ = Copyright (c) 2023 Evert Provoost. See LICENSE.
+ =
+ = Provided functionality is partially inspired by
+ =
+ =     Wegert, Elias. Visual Complex Functions:
+ =       An Introduction with Phase Portraits.
+ =       Birkhäuser Basel, 2012.
+ =#
 
 module DomainColoring
 
@@ -15,8 +16,42 @@ using GLMakie, ColorTypes
 
 export domaincolor, checkerplot
 
+"""
+    DomainColoring.shadedplot(
+        f :: Complex -> Complex,
+        shader :: (Complex; kwargs...) -> Color,
+        axes = (-1, 1, -1, 1);
+        pixels = (720, 720),
+        kwargs...
+    )
 
-function shadedplot(f, shader, axes=(-1, 1, -1, 1); pixels=(720, 720), kwargs...)
+Takes a complex function and a shader and produces a Makie image plot.
+
+# Arguments
+
+**`f`** is the complex function to plot.
+
+**`shader`** is the shader function to compute pixel colors.
+
+**`axes`** are the limits of the rectangle to plot, in the format
+`(min Re, max Re, min Im, max Im)`, if one or two numbers are provided
+instead they are take symmetric along the real and imaginary axis.
+
+# Keyword Arguments
+
+**`pixels`** is the number of pixels to compute in, respectively, the
+real and imaginary axis, taking the same for both if only one number is
+provided.
+
+The remaining keyword arguments are passed to the **`shader`** function.
+"""
+function shadedplot(
+        f,
+        shader,
+        axes = (-1, 1, -1, 1);
+        pixels = (720, 720),
+        kwargs...
+    )
     length(axes) == 1 && (axes = (-axes, axes, -axes, axes))
     length(axes) == 2 && (axes = (-axes[1], axes[1], -axes[2], axes[2]))
     length(pixels) == 1 && (pixels = (pixels,pixels))
@@ -28,7 +63,26 @@ function shadedplot(f, shader, axes=(-1, 1, -1, 1); pixels=(720, 720), kwargs...
 end
 
 
-function domaincolor_shader(w; abs=false, logabs=false, grid=false, all=false)
+"""
+    DomainColoring.domaincolorshader(
+        w :: Complex;
+        abs = false,
+        logabs = false,
+        grid = false,
+        all = false,
+    )
+
+Takes a complex value **`w`** and shades it as in a domain coloring.
+
+For documentation of the remaining arguments see [`domaincolor`](@ref).
+"""
+function domaincolorshader(
+        w;
+        abs = false,
+        logabs = false,
+        grid = false,
+        all = false,
+    )
     all && (abs = true; grid = true)
     logabs && (abs = true)
 
@@ -53,14 +107,65 @@ function domaincolor_shader(w; abs=false, logabs=false, grid=false, all=false)
     return c
 end
 
-domaincolor(f, axes=(-1,1,-1,1); kwargs...) = shadedplot(f, domaincolor_shader, axes; kwargs...)
+"""
+    domaincolor(
+        f :: Complex -> Complex,
+        axes = (-1, 1, -1, 1);
+        pixels = (720, 720),
+        abs = false,
+        logabs = false,
+        grid = false,
+        all = false,
+    )
+
+Takes a complex function and produces it's domain coloring as a Makie
+image plot.
+
+!!! todo
+    Document arguments.
+"""
+function domaincolor(
+        f,
+        axes = (-1, 1, -1, 1);
+        pixels = (720, 720),
+        abs = false,
+        logabs = false,
+        grid = false,
+        all = false,
+    )
+    shadedplot(f, domaincolorshader, axes; pixels,
+               abs, logabs, grid, all)
+end
 
 
-function checkerplot_shader(w; angle=false, abs=false, rect=false, real=false, imag=false)
+"""
+    DomainColoring.checkerplotshader(
+        w :: Complex;
+        real = true,
+        imag = true,
+        angle = false,
+        abs = false,
+        polar = false,
+    )
+
+Takes a complex value **`w`** and shades it as in a checker plot.
+
+For documentation of the remaining arguments see [`checkerplot`](@ref).
+"""
+function checkerplotshader(
+        w;
+        real = false,
+        imag = false,
+        angle = false,
+        abs = false,
+        polar = false,
+    )
     # set defaults if no options given
-    !(angle || abs || rect || real || imag) && (angle = true; abs = true)
-    # rectangual grid
-    rect && (real = true; imag = true)
+    if !(angle || abs || polar || real || imag)
+        real = true; imag = true
+    end
+    # polar checker plot
+    polar && (angle = true; abs = true)
 
     g = 1.0
     angle && (g *= sin(15Base.angle(w)))
@@ -71,6 +176,35 @@ function checkerplot_shader(w; angle=false, abs=false, rect=false, real=false, i
     return Gray(0.9min(1, sign(g) + 1) + 0.08)
 end
 
-checkerplot(f, axes=(-1,1,-1,1); kwargs...) = shadedplot(f, checkerplot_shader, axes; kwargs...)
+"""
+    checkerplot(
+        f :: Complex -> Complex,
+        axes = (-1, 1, -1, 1);
+        pixels = (720, 720),
+        real = true,
+        imag = true,
+        angle = false,
+        abs = false,
+        polar = false,
+    )
+
+Takes a complex function and produces a checker plot as a Makie image.
+
+!!! todo
+    Document arguments.
+"""
+function checkerplot(
+        f,
+        axes = (-1, 1, -1, 1);
+        pixels = (720, 720),
+        real = false,
+        imag = false,
+        angle = false,
+        abs = false,
+        polar = false,
+    )
+    shadedplot(f, checkerplotshader, axes; pixels,
+               real, imag, angle, abs, polar)
+end
 
 end
