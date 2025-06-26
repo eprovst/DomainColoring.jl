6e85b00fcef28923efba84d278ec8e3eb465547c
Evert Provoost <evert.provoost@kuleuven.be>
2025-06-26 15:02:55 +0200

    Make DomainColoring compatible with Makie 0.24+

--- ext/MakieExt.jl
+++ ext/MakieExt.jl
@@ -1,7 +1,7 @@
 module MakieExt
 
 import DomainColoring as DC
-import MakieCore as MC
+import Makie
 
 # Makie specific version of `shadedplot` and `shadedplot!`
 for (modifying, target) in
@@ -26,21 +26,21 @@ for (modifying, target) in
 
             # parse Makie options
             $(if !modifying
-                  :(defaults = MC.Attributes(;
+                  :(defaults = Makie.Attributes(;
                       interpolate = true,
                       axis = (autolimitaspect = 1,
                               aspect = (limits[2] - limits[1]) /
                                        (limits[4] - limits[3]))))
               else
-                  :(defaults = MC.Attributes(; interpolate = true))
+                  :(defaults = Makie.Attributes(; interpolate = true))
               end)
 
-            attr = merge(MC.Attributes(; kwargs...), defaults)
+            attr = merge(Makie.Attributes(; kwargs...), defaults)
 
             # images have inverted y and flip x and y in their storage
             r = [limits[1], limits[2]]
             i = [limits[4], limits[3]]
-            MC.$hname($(target...), r, i,
+            Makie.$hname($(target...), r, i,
                       DC.renderimage(f, shader, limits, pixels)';
                       attr...)
         end
