4f6edfc1836f48e7ffeb6f4e806c8a673502f6f6
Documenter.jl <documenter@juliadocs.github.io>
2026-09-22 12:34:44 +0000

    build based on 56378fb

--- dev/assets/render_logo.jl
+++ dev/assets/render_logo.jl
@@ -1,4 +1,4 @@
 using Images
 using DomainColoring
 
-save("logo.png", domaincolorimg(z -> im*(z+.1im)^3-1, 2.5; all=true))
+save("logo.png", domaincolorimg(z -> im*(z+.1im)^3-1, 2.5; all=true, aa=4))
